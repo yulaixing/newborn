078ca47818899984a79a41b364446775d2c1a87d
xingyulai <xingyulai@kanzhun.com>
2018-10-15 16:27:29 +0800

    添加logback,其实logback就是log4j的升级版

---
+++ kanzhun-logbacktest/src/main/java/com/kanzhun/LogBackTest.java
@@ -0,0 +1,20 @@
+package com.kanzhun;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author:xing.yl
+ * @date: 18/10/15
+ */
+public class LogBackTest {
+
+   private static Logger logger = LoggerFactory.getLogger(LogBackTest.class);
+
+    public static void main(String[] args) {
+
+
+        logger.info("first blood");
+    }
+
+}
